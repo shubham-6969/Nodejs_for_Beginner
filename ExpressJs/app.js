ba6636f33df2362e8de68a7bc3091d17a301bb6e
Shubham Patwal <shubhampatwal695@gmail.com>
2025-09-08 11:42:03 +0530

    file added

--- ExpressJs/app.js
+++ ExpressJs/app.js
@@ -6,11 +6,28 @@ Start the server (app.listen()) */
 
 
 import express from 'express';
+import {z} from "zod";
 
 // Creating instance of express
 const app = express();
+app.use(express.json())
+const PORT = process.env.PORT || 3000;
 
-const PORT = 3000
+// Define schema
+
+const userSchema = z.object({
+    name: z.string(),
+    age: z.number().min(18) // age must be number >=18
+});
+
+app.post("/user", (req, res)=> {
+    try {
+        const data = userSchema.parse(req.body); // validate request body
+        // if data valid then this line will excute forward.
+    } catch (error) {
+        res.status(400).json({error: error.errors})
+    }
+});
 
 app.get("/", (req, res)=> {
     res.send("Mitroooo")
